1e7e8ac37de4d164f5a60d2f784710d48f4ec66e
haaag <haag_@tutanota.com>
2022-07-14 18:43:20 -0300

    update settings/urls

--- coder_market/urls.py
+++ coder_market/urls.py
@@ -4,11 +4,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-import carrito.views as cart
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("", include("carrito.urls")),
-    path("add_to_cart/<int:product_id>", cart.add_to_cart, name="add_to_cart"),
+    path("", include("cursos.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
